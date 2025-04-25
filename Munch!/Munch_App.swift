a10d77fbea9d748905d19a51c0b6148ffeaab652
Andrew <arg286@cornell.edu>
2025-04-25 00:29:59 -0400

    Initial Commit

---
+++ Munch!/Munch_App.swift
@@ -0,0 +1,17 @@
+//
+//  Munch_App.swift
+//  Munch!
+//
+//  Created by Andrew Gao on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Munch_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Munch!Tests/Munch_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Munch_Tests.swift
+//  Munch!Tests
+//
+//  Created by Andrew Gao on 4/25/25.
+//
+
+import Testing
+@testable import Munch_
+
+struct Munch_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
